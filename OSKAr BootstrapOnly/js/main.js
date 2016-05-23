225ca6b84e416988726d4a580d4650699d6befc7
Sebastian Röhling <se.roehling@gmx.de>
2016-05-23 17:22:56 +0200

    finished gallery

--- OSKAr BootstrapOnly/js/main.js
+++ OSKAr BootstrapOnly/js/main.js
@@ -38,17 +38,17 @@ jQuery(function($) {
 
 	//Isotope
 	$(window).load(function(){
-		$portfolio = $('.portfolio-items');
-		$portfolio.isotope({
+		$gallery = $('.gallery-items');
+		$gallery.isotope({
 			itemSelector : 'li',
 			layoutMode : 'fitRows'
 		});
-		$portfolio_selectors = $('.portfolio-filter >li>a');
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
+		$gallery_selectors = $('.gallery-filter >li>a');
+		$gallery_selectors.on('click', function(){
+			$gallery_selectors.removeClass('active');
 			$(this).addClass('active');
 			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
+			$gallery.isotope({ filter: selector });
 			return false;
 		});
 	});
